07d3d990f23443de357813e62ea90713eda17bf2
tesirm99 <tesiroblesmarin@gmail.com>
2024-05-22 15:10:13 +0200

    modified package.jsons to publish my own package

--- publish/dist/indexes/AdapterMinio.d.ts
+++
@@ -1,5 +0,0 @@
-export { AdapterMinio } from "../AdapterMinio";
-export * from "../types/adapter_minio";
-export * from "../types/general";
-export * from "../types/result";
-export * from "../types/add_file_params";
--- publish/dist/indexes/Storage.d.ts
+++
@@ -1,3 +0,0 @@
-export { Storage } from "../Storage";
-export * from "../adapters";
-export { StorageType, IAdapter, AdapterConfig, StorageAdapterConfig } from "../types/general";
